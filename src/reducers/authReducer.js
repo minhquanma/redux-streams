902a21ef9ac40355a3cf0bd3cfbfb4dc15433628
minhquanma <minhquanma@gmail.com>
2019-02-10 23:13:30 +0700

    First commit

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,17 @@
+import { SIGN_OUT } from '../actions/types';
+import { SIGN_IN } from './../actions/types';
+
+const INITIAL_STATE = {
+  isSignedIn: null
+};
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
+    default:
+      return state;
+  }
+};
