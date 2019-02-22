4c4a1672a896551f97d0ceea23a1fdaae8b3e28b
Quan <quanmm@necsv-hcm.com>
2019-02-22 10:09:17 +0700

    Lecture 262

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -30,7 +30,7 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            k{stream.title}
+            {stream.title}
             <div className="description">{stream.description}</div>
           </div>
         </div>
